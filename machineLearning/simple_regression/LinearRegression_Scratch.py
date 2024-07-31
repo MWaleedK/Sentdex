0b1352dae4a77e871d791685965adedfe1882492
VulcanBin <waleedjaved911@gmail.com>
2024-07-31 14:27:53 +0500

    r-Squared Error added

--- machineLearning/simple_regression/LinearRegression_Scratch.py
+++ machineLearning/simple_regression/LinearRegression_Scratch.py
@@ -24,6 +24,22 @@ def best_fit_slope_and_intercept(xs, ys):
     b = mean(ys) - m*mean(xs)
     return m, b
 
+
+#squared error would be (generated output - actual output)^2 
+
+def squared_error(ys_orig, ys_line):
+    return sum((ys_line - ys_orig) **2)
+
+
+#Finding r^2 is equal to 1 - ( SE(generated output))  / ( SE(mean of the actual output) ))
+
+def coeffieciet_of_determination(ys_orig, ys_line):
+    ys_mean_line = [mean(ys_orig) for y in ys_orig]
+    sqaured_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_mean = squared_error(ys_orig, ys_mean_line)
+    return 1 - (sqaured_error_regr / squared_error_mean)
+
+
 m,b = best_fit_slope_and_intercept(xs, ys)
 
 
@@ -34,7 +50,9 @@ regression_line = [(m*x) + b for x in xs] # regression_line is y as y=mx+b, that
 predict_x = 8
 predict_y = (m*predict_x) + b
 
+r_squared = coeffieciet_of_determination(ys, regression_line)
 
+print(r_squared)
 plt.scatter(xs,ys)
 plt.scatter(predict_x,predict_y, color='g')
 plt.plot(xs,regression_line)
